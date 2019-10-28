124377e9759b31ed6c65a781fbe378a8e1e88de9
Rick Lim <rick.jason.a.lim@gmail.com>
2019-10-28 04:05:10 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,68 @@
 def create_an_empty_array
-  
+[]
 end
+
+p create_an_empty_array
 
 def create_an_array
-  
+  create_an_array = ["Liverpool", "Man City", "Leicester", "Chelsea"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+p create_an_array
+
+def add_element_to_end_of_array
+  add_element_to_end_of_array = ["Liverpool", "Man City", "Leicester", "Chelsea"]
+  add_element_to_end_of_array.push("Arsenal")
 end
 
-def add_element_to_start_of_array(array, element)
-  
+p add_element_to_end_of_array
+
+def add_element_to_start_of_array
+  add_element_to_start_of_array = ["Man City", "Leicester", "Chelsea"]
+  add_element_to_start_of_array.unshift("Liverpool")
 end
 
-def remove_element_from_end_of_array(array)
-  
+p add_element_to_start_of_array
+
+def premier_leagues
+  premier_leagues = ["Liverpool", "Man City", "Leicester", "Chelsea"]
+  chelsea_league = premier_leagues.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+p premier_leagues
+
+
+def premier_league
+  premier_league = ["Man Utd", "Liverpool", "Man City", "Leicester"]
+  man_utd = premier_league.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+p premier_league
+
+def retrieve_element_from_index
+  retrieve_element_from_index = ["Liverpool", "Man City", "Leicester", "Chelsea"]
+  retrieve_element_from_index[2]
 end
 
-def retrieve_first_element_from_array(array)
-  
+p retrieve_element_from_index
+
+def retrieve_first_element_from_array
+  retrieve_first_element_from_array = ["Liverpool", "Man City", "Leicester", "Chelsea"]
+  retrieve_first_element_from_array[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+p retrieve_first_element_from_array
+
+def retrieve_last_element_from_array
+  retrieve_last_element_from_array = ["Liverpool", "Man City", "Leicester", "Chelsea"]
+  retrieve_last_element_from_array[-1]
 end
 
-def update_element_from_index(array, index_number, element)
+p retrieve_last_element_from_array
 
+def update_element_from_index
+update_element_from_index = ["Liverpool", "Man City", "Leicester", "Arsenal"]
+update_element_from_index[-1] = "Chelsea"
 end
+
+p update_element_from_index
